4caf158bd03f8c1191ef9b3e1b97bb43ff6a5029
Alex Ravan <alexravan@gmail.com>
2016-10-14 22:43:18 -0400

    tweeets

--- get_tweets.py
+++ get_tweets.py
@@ -1,5 +1,7 @@
-from twython import Twython # pip install twython
+from twython import Twython, TwythonError # pip install twython
 import time # standard lib
+import csv
+import sys
 
 ''' Go to https://apps.twitter.com/ to register your app to get your api keys '''
 CONSUMER_KEY = 'a2bLpVe5RKCp3miLncRSJj4uv'
@@ -8,13 +10,51 @@ ACCESS_KEY = '1395752780-GVCvpoVgCmyrhLKknaIyICwH9bDETpUem2JQ9HC'
 ACCESS_SECRET = '3kn6YVXULEy5HGsG7EuwcROuIWKH2fUeSMOiQ6f3l4o4z'
 
 twitter = Twython(CONSUMER_KEY,CONSUMER_SECRET,ACCESS_KEY,ACCESS_SECRET)
-lis = [467020906049835008] ## this is the latest starting tweet id
-for i in range(0, 16): ## iterate through all tweets
-## tweet extract method with the last list item as the max_id
-    user_timeline = twitter.get_user_timeline(screen_name="kanyewest",
-    count=200, include_retweets=False, max_id=lis[-1])
-    time.sleep(300) ## 5 minute rest between api calls
 
-    for tweet in user_timeline:
-        print tweet['text'] ## print the tweet
-        lis.append(tweet['id']) ## append tweet id's
+lis = [467020906049835008] ## this is the latest starting tweet id
+filepath = "tweets/" + sys.argv[2]
+calls = 1
+
+for i in range(0, 16): ## iterate through all tweets
+	f = open(filepath, 'a')
+## tweet extract method with the last list item as the max_id
+#     user_timeline = twitter.get_user_timeline(screen_name="sys.argv[1]",
+#     count=200, include_retweets=False, max_id=lis[-1])
+	try:
+	    user_timeline = twitter.get_user_timeline(screen_name=sys.argv[1], count=200, include_retweets=False, max_id=lis[-1])
+	except TwythonError as e:
+	   print e
+
+	try:
+	    writer = csv.writer(f)
+	    for tweet in user_timeline:
+			writer.writerow( [tweet['text'].encode('utf-8')] )	
+			lis.append(tweet['id'])
+	finally:
+		f.close()
+	if calls == 5:
+		time.sleep(300)
+		calls = 0
+	else:
+		calls += 1
+		
+
+
+    # for tweet in user_timeline:
+
+#         # print tweet['text'] ## print the tweet
+#         lis.append(tweet['id']) ## append tweet id's
+# try:
+#     user_timeline = twitter.get_user_timeline(screen_name=sys.argv[1], count=3000)
+# except TwythonError as e:
+#    print e
+
+# # filepath = "tweets/" + sys.argv[2]
+# # f = open(filepath, 'wt')
+# try:
+#     writer = csv.writer(f)
+#     for tweets in user_timeline:
+# 		writer.writerow( [tweets['text'].encode('utf-8')] )	
+# finally:	
+# 	f.close()
+
